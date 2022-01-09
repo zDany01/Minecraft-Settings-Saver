fd085cea9bd89121abbf13965960abf62a43608a
BigSaw11 <97407372+BigSaw11@users.noreply.github.com>
2022-01-09 20:41:35 +0100

    removed useful code

--- MinecraftSettingsSaver/Program.cs
+++ MinecraftSettingsSaver/Program.cs
@@ -8,9 +8,6 @@ namespace MinecraftSettingsSaver
 {
     internal static class Program
     {
-        /// <summary>
-        /// Punto di ingresso principale dell'applicazione.
-        /// </summary>
         [STAThread]
         static void Main()
         {
