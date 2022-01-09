e27650ad16f438152bfe6918784b527be2459c6d
BigSaw11 <97407372+BigSaw11@users.noreply.github.com>
2022-01-09 20:42:55 +0100

    :)

--- MinecraftSettingsSaver/Properties/AssemblyInfo.cs
+++ MinecraftSettingsSaver/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MinecraftSettingsSaver")]
-[assembly: AssemblyCopyright("Copyright © zDany01 2022")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
